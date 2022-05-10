9f86e57c8e8fcaed6e1a881a84947e97f133b997
Nahuel Kisser <81387542+kisser91@users.noreply.github.com>
2022-05-10 18:37:27 -0300

    babel fixed

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,17 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import state from "../../src/store/state";
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+describe("HelloWorld.vue", () => {
+  it("initial State ", () => {
+    const initialState = {
+      theme: {
+        theme: "dark",
+        bool: true,
+      },
+      userResponse: null,
+      reportResponse: null,
+      loginMenu: false,
+    };
+
+    expect(state).toEqual(initialState);
+  });
+});
