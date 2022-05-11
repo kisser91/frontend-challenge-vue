5c52edef8783f29c1de0cc9f8cd9ff38528eb83f
Nahuel Kisser <81387542+kisser91@users.noreply.github.com>
2022-05-11 16:21:58 -0300

    more tests

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import getters from "./store/getters";
 import actions from "./store/actions";
 import mutations from "./store/mutations";
 import Vuex from "vuex";
+import "@babel/polyfill";
 Vue.config.productionTip = false;
 
 if (process.env.NODE_ENV === "development") makeServer();
